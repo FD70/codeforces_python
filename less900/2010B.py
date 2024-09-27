4cad60798fa707dfea081845390d0c9497300a25
FD70000000 <halfl.ez.ser@mail.ru>
2024-09-27 17:50:47 +0300

    add 2010B

---
+++ less900/2010B.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/2010/B
+
+# _input = "3 1"
+
+if __name__ == '__main__':
+    # input_list = list(map(int, input().split()))
+    # sum(map(int, _input.split()))
+    # 1 + 2 + 3 == 6:
+    # print(6 - sum(map(int, _input.split())))
+    print(6 - sum(map(int, input().split())))
